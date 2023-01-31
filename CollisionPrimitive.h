158c2fb2e144294f60bcf06c7ad035614336f374
sasagawahikaru <k021g1383@g.neec.ac.jp>
2023-01-31 10:39:41 +0900

    プロジェクト ファイルを追加します。

---
+++ CollisionPrimitive.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<DirectXMath.h>
+struct Sphere
+{
+	DirectX::XMVECTOR center = { 0,0,0,1 };
+	float radius = 1.0f;
+};
+struct Plane
+{
+	DirectX::XMVECTOR normal = { 0,1,0,0 };
+	float distance = 0.0f;
+};
